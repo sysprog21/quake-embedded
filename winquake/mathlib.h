1c38a40dbc122cb541379167e310fa1b061b0136
Jim Huang <jserv@biilabs.io>
2022-08-23 14:10:35 +0800

    Rewrite IS_NAN to avoid violating strict aliasing rules

--- winquake/mathlib.h
+++ winquake/mathlib.h
@@ -43,8 +43,13 @@ struct mplane_s;
 
 extern vec3_t vec3_origin;
 
-#define NAN_MASK 0x7f800000
-#define	IS_NAN(x) (((*(int *)&x) & NAN_MASK) == NAN_MASK)
+#define NAN_MASK (255 << 23)	/* 0x7F800000 */
+static inline int IS_NAN (float x)
+{
+	union { float f; int i; } num;
+	num.f = x;
+	return ((num.i & NAN_MASK) == NAN_MASK);
+}
 
 #define DotProduct(x,y) (x[0]*y[0]+x[1]*y[1]+x[2]*y[2])
 #define VectorSubtract(a,b,c) {c[0]=a[0]-b[0];c[1]=a[1]-b[1];c[2]=a[2]-b[2];}
