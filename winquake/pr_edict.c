7d0cded83795b1b89ae9415f7bb733f255f615ac
Jim Huang <jserv@ccns.ncku.edu.tw>
2024-08-20 21:39:03 +0800

    Fix triggraphs warning

--- winquake/pr_edict.c
+++ winquake/pr_edict.c
@@ -390,7 +390,7 @@ char *PR_GlobalString (int ofs)
 	val = (void *)&pr_globals[ofs];
 	def = ED_GlobalAtOfs(ofs);
 	if (!def)
-		sprintf (line,"%i(???)", ofs);
+		sprintf (line,"%i(\?\?\?)", ofs);
 	else
 	{
 		s = PR_ValueString (def->type, val);
@@ -413,7 +413,7 @@ char *PR_GlobalStringNoContents (int ofs)
 	
 	def = ED_GlobalAtOfs(ofs);
 	if (!def)
-		sprintf (line,"%i(???)", ofs);
+		sprintf (line,"%i(\?\?\?)", ofs);
 	else
 		sprintf (line,"%i(%s)", ofs, pr_strings + def->s_name);
 	
